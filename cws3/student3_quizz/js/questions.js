1860f872f92d38f86a6cf2e623aa41df7aeb77ff
Hashani Navodya <129249685+Hashani-navodya@users.noreply.github.com>
2024-01-08 18:45:41 +0530

    Initial commit

---
+++ cws3/student3_quizz/js/questions.js
@@ -0,0 +1,117 @@
+//creating an array and passing the number, questions, options and answers
+let questions =[
+    {
+        numb: 1,
+        question: "Which is largest animal in the world?",
+        answer: "Blue whale",
+        options: [
+            "Shark",
+            "Blue whale",
+            "Elephant",
+            "Giraffe"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the first element in the periodic table?",
+        answer: "Hydrogen",
+        options: [
+            "Lithium",
+            "Oganesson",
+            "Helium",
+            "Hydrogen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the largest organ in the human body?",
+        answer: "Skin",
+        options: [
+            "Skin",
+            "Kidney",
+            "Liver",
+            "Heart"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who is the first person to set on foot on the moon?",
+        answer: "Neil Amstrong",
+        options: [
+            "John Glein",
+            "Isec Newton",
+            "Neil Amstrong",
+            "Yuri Gagarin"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which is smallest continent in the world?",
+        answer: "Australiya",
+        options: [
+            "Arctic",
+            "Asia",
+            "Africa",
+            "Australiya"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Who is the first man in space?",
+        answer: "Yuri Gagarin",
+        options: [
+            "Edwin Auldrin",
+            "Neil Amstrong",
+            "Yuri Gagarin",
+            "Galiliyo Galily"
+            
+        ]
+    },
+    {
+        numb: 7,
+        question: "Who is credited with the plum pudding model?",
+        answer: "J.J. Thomson",
+        options: [
+            "J.J. Thomson",
+            "Isec Newton",
+            "Mary Cury",
+            "John Dolton"
+        ]
+    },
+    {
+        numb: 8,
+        question: "what is the largest planet in our solar system?",
+        answer: "Jupiter",
+        options: [
+            "Earth",
+            "Pluto",
+            "Jupiter",
+            "Mars"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Who discoverd radium and polonium?",
+        answer: "Mary Cury",
+        options: [
+            "Isec Newton",
+            "Mary Cury",
+            "John Dolton",
+            "J.J. Thomson"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the largest forest on Earth?",
+        answer: "Amazon rainforest",
+        options: [
+            "Gir forest",
+            "Sinharaja forest",
+            "Daintree forest",
+            "Amazon rainforest"
+        ]
+    },
+    
+   
+   
+]
